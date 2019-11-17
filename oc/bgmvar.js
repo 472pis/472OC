847b643e8d6eb280361fa28f1491d06530b7767b
472pis <57526098+472pis@users.noreply.github.com>
2019-11-18 07:11:23 +0900

    Add files via upload

---
+++ oc/bgmvar.js
@@ -0,0 +1,15 @@
+var setting = {
+
+"candela" : "_shWlfgnzZw,#8ac7ff,#ab3025,1,blue, style='font-size:1.5rem;'",
+"lux" : "6U-ClAlzwZ4,#feff8b,#100658,2,hotpink,",
+"bubble" : "Nr8RU5cSct0,black,white,3,powderblue,",
+"gm" : "AsJnL5rgOac,#f1f1f1,black,4,#ff3c00,",
+"wifi" : "pKr1N8JW2j8,#292929,white,5,#7c85a0,",
+"clear" : "q6p6sx4v7D0,white,black,6,purple,",
+"yuta" : "m_jOT8qIhVE,white,#5d5d5d,7,#a8d65f,",
+"nothing" : "aDlNMawir4o,#292929,white,8,red, style='font-size:1.5rem;'",
+"z472" : "aDlNMawir4o,white,black,9",
+};
+
+
+
